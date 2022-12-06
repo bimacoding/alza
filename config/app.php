97dad12d83317abfde6d5fe71c31da9e9d1cdf15
bimacoding <bimacoding@gmail.com>
2022-12-06 09:48:03 +0700

    Alza Simple Framework

--- config/app.php
+++ config/app.php
@@ -115,4 +115,23 @@ return [
 
     'https' => env('HTTPS', false),
 
+    'providers' => [
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
+
+    'aliases' => [
+
+        
+    ],
 ];
